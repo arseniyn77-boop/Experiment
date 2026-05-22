f0090bfeef762f88b234f0c2173b82c48fa447af
Колесников А.Ю. <arseniyn77@gmail.com>
2026-05-23 01:21:58 +0300

    js_2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ const STATUSES = {
 };
 
 function App() {
-
+// 1. Инициализация состояния
   const [experiments, setExperiments] = useState(() => {
     const saved = localStorage.getItem('experiments');
     return saved ? JSON.parse(saved) : [];
@@ -17,6 +17,10 @@ function App() {
   const [status, setStatus] = useState(STATUSES.PLAN);
   const [filter, setFilter] = useState('Все');
 
+  // 2. Сохранение данных
+  useEffect(() => {
+    localStorage.setItem('experiments', JSON.stringify(experiments));
+  }, [experiments]);
 
 }
 
